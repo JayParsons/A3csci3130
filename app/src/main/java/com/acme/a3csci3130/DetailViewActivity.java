362e367c30985860b1980467067bea8d9b676f55
JayParsons <jason.parsons@dal.ca>
2017-07-11 09:21:46 -0300

    FInal version

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -8,7 +8,8 @@ import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
+    private EditText nameField, businessNumberField, primaryBusinessField, addressField, provinceField;
+    private MyApplicationData appState;
     Contact receivedPersonInfo;
 
     @Override
@@ -18,20 +19,38 @@ public class DetailViewActivity extends Activity {
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        businessNumberField = (EditText) findViewById(R.id.businessNumber);
+        primaryBusinessField = (EditText) findViewById(R.id.primaryBusiness);
+        addressField = (EditText) findViewById(R.id.address);
+        provinceField = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            businessNumberField.setText(receivedPersonInfo.businessNumber);
+            primaryBusinessField.setText(receivedPersonInfo.primaryBusiness);
+            addressField.setText(receivedPersonInfo.address);
+            provinceField.setText(receivedPersonInfo.province);
         }
     }
 
     public void updateContact(View v){
-        //TODO: Update contact funcionality
+        appState = ((MyApplicationData) getApplicationContext());
+        String newUID = receivedPersonInfo.uid;
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        String name = nameField.getText().toString();
+        String businessNumber = businessNumberField.getText().toString();
+        String primaryBusiness = primaryBusinessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+        Contact person = new Contact(newUID, name, businessNumber, primaryBusiness, address, province);
+        appState.firebaseReference.child(newUID).setValue(person);
+        finish();
     }
 
     public void eraseContact(View v)
     {
-        //TODO: Erase contact functionality
+        appState = ((MyApplicationData) getApplicationContext());
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        finish();
     }
 }
